924be9190fb060e5baae17079f5092eb287aa53f
ispan <ispan@DESKTOP-PAKSETB>
2022-11-09 11:11:00 +0800

    加入專案檔案。

---
+++ Exec3_Enumerable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec3_Enumerable
+{
+    /*
+     * 利用Enumerable.Range() 建立一個序列, 值介於 [1, 10]
+     * 請利用迴圈列出每一個項目值,確認無誤
+     * 
+     * 用Enumerable.Range() 建立一個序列, 值介於 [10, 15]
+     * 請利用迴圈列出每一個項目值,確認無誤
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> ints = Enumerable.Range(1, 10);
+            foreach (var item in ints)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            ints = Enumerable.Range(10, 6);
+            foreach (var item in ints)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
